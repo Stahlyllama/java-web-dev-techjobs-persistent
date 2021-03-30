ceb4df61351ddb0eff24e59ea36945d90d71b76e
Kim Stahl <kim.stahl@gmail.com>
2021-03-30 11:20:21 -0500

    this is my first and final commit.  I had to start over 3 times to get rid of error messages and had many more commits in other projects.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message= "please enter a location!")
+    @Size(min=3, max=100)
+    private String location;
 
+    @OneToMany
+    @JoinColumn()
+    private List<Job> jobs = new ArrayList<>();
 
-}
+    public Employer (){}
+
+    public String getLocation() { return location;}
+
+    public void setLocation(String location) {this.location = location;}
+
+    }
